39d29a4b3be983ed5904899981f36c84381b5b5b
Kenta Murata <mrkn@mrkn.jp>
2020-06-03 15:25:57 +0900

    Rewrite an example

--- spec/pandas/data_frame_spec.rb
+++ spec/pandas/data_frame_spec.rb
@@ -2,28 +2,27 @@ require 'spec_helper'
 
 module Pandas
   ::RSpec.describe DataFrame do
+    let(:values) do
+      [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+      ]
+    end
+
+    subject(:df) do
+      Pandas::DataFrame.new(values, index: %w[r1 r2 r3], columns: %w[x1 x2 x3 x4])
+    end
+
     describe '#[key]' do
       context 'When the key is an Array' do
         specify do
-          df = Pandas.read_csv(file_fixture('people.csv').to_s)
-          expect(df[['name', 'sex']].iloc[0]).to eq(['matz', 'male'])
+          expect(df[["x1", "x3"]].iloc[0]).to eq([1, 3])
         end
       end
     end
 
     describe '#loc[key]' do
-      let(:values) do
-        [
-          [1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12]
-        ]
-      end
-
-      subject(:df) do
-        Pandas::DataFrame.new(values, index: %w[r1 r2 r3], columns: %w[x1 x2 x3 x4])
-      end
-
       context 'When the key is a String' do
         specify do
           expect(df.loc["r3"]).to eq([9, 10, 11, 12])
