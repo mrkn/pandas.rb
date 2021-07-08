337bb5a2d33459abe6df47611dc90bee7e49fd67
Kenta Murata <mrkn@mrkn.jp>
2021-07-08 17:42:45 +0900

    Add to_iruby_mimebundle in DataFrame

--- lib/pandas/data_frame.rb
+++ lib/pandas/data_frame.rb
@@ -10,5 +10,25 @@ module Pandas
     def to_narray
       to_numpy.to_narray
     end
+
+    def to_iruby_mimebundle(include: [], exclude: [])
+      include = ["text/html", "text/latex", "text/plain"] if include.empty?
+      include -= exclude unless exclude.empty?
+      include.map { |mime|
+        data = case mime
+               when "text/html"
+                 _repr_html_
+               when "text/latex"
+                 _repr_latex_
+               when "text/plain"
+                 if respond_to?(:_repr_pretty_)
+                   _repr_pretty_
+                 else
+                   __repr__
+                 end
+               end
+        [mime, data] if data
+      }.compact.to_h
+    end
   end
 end
